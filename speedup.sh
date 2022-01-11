9db204b7ed8c8eaca83f9fce61312249c977f13e
ValdikSS <iam@valdikss.org.ru>
2022-01-11 17:34:35 +0300

    Use eatmydata for apt and dpkg instead of force-unsafe-io in dpkg

--- speedup.sh
+++ speedup.sh
@@ -6,3 +6,9 @@ sudo sed '/install-info/d' -i /var/lib/dpkg/triggers/File
 sudo sed '/mime/d' -i /var/lib/dpkg/triggers/File
 sudo sed '/hicolor-icon-theme/d' -i /var/lib/dpkg/triggers/File
 echo "force-unsafe-io" | sudo tee -a /etc/dpkg/dpkg.cfg.d/force-unsafe-io
+if [ -a /usr/bin/eatmydata ]; then
+  echo "eatmydata available"
+  echo -e '#!/bin/sh\nexec eatmydata /usr/bin/dpkg $@' | sudo tee /usr/local/bin/dpkg && sudo chmod +x /usr/local/bin/dpkg
+  echo -e '#!/bin/sh\nexec eatmydata /usr/bin/apt $@' | sudo tee /usr/local/bin/apt && sudo chmod +x /usr/local/bin/apt
+  echo -e '#!/bin/sh\nexec eatmydata /usr/bin/apt-get $@' | sudo tee /usr/local/bin/apt-get && sudo chmod +x /usr/local/bin/apt-get
+fi
